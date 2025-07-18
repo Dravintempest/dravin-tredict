005a9850d7c1b05fbfc8a87c5fb70bf54d849843
Dravin <dravinnnnn@gmail.com>
2025-07-18 19:06:31 +0700

    Add files via upload

---
+++ Penyimpanan/Enchard.js
@@ -0,0 +1,124 @@
+// Module... 
+
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  downloadContentFromMessage,
+  emitGroupParticipantsUpdate,
+  emitGroupUpdate,
+  generateWAMessageContent,
+  generateWAMessage,
+  makeInMemoryStore,
+  prepareWAMessageMedia,
+  generateWAMessageFromContent,
+} = require("@whiskeysockets/baileys");
+const {
+  default: MediaType,
+  areJidsSameUser,
+  WAMessageStatus,
+  downloadAndSaveMediaMessage,
+  AuthenticationState,
+  GroupMetadata,
+  initInMemoryKeyStore,
+  getContentType,
+  MiscMessageGenerationOptions,
+  useSingleFileAuthState,
+  BufferJSON,
+  WAMessageProto,
+  MessageOptions,
+  WAFlag,
+  WANode,
+  WAMetric,
+  ChatModification,
+  MessageTypeProto,
+  WALocationMessage,
+  ReconnectMode,
+  WAContextInfo,
+  proto,
+  WAGroupMetadata,
+  ProxyAgent,
+  waChatKey,
+  MimetypeMap,
+  MediaPathMap,
+  WAContactMessage,
+  WAContactsArrayMessage,
+  WAGroupInviteMessage,
+  WATextMessage,
+  WAMessageContent,
+  WAMessage,
+  BaileysError,
+  WA_MESSAGE_STATUS_TYPE,
+  MediaConnInfo,
+  URL_REGEX,
+  WAUrlInfo,
+  WA_DEFAULT_EPHEMERAL,
+  WAMediaUpload,
+  jidDecode,
+  mentionedJid,
+  processTime,
+  Browser,
+  MessageType,
+  Presence,
+  WA_MESSAGE_STUB_TYPES,
+  Mimetype,
+  relayWAMessage,
+  Browsers,
+  GroupSettingChange,
+  DisconnectReason,
+  WASocket,
+  getStream,
+  WAProto,
+  isBaileys,
+  AnyMessageContent,
+  fetchLatestBaileysVersion,
+  templateMessage,
+  InteractiveMessage,
+  Header,
+} = require("@whiskeysockets/baileys");
+
+let sock;
+
+
+// Function... 
+const getMvaaObfuscationConfig = () => {
+  const generateSiuCalcrickName = () => {
+    // Identifier generator pseudo-random tanpa crypto
+    const chars =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let randomPart = "";
+    for (let i = 0; i < 6; i++) {
+      // 6 karakter untuk keseimbangan
+      randomPart += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return `和无气MVAANICH和无气${randomPart}`;
+  };
+
+  return {
+    target: "node",
+    compact: true,
+    renameVariables: true,
+    renameGlobals: true,
+    identifierGenerator: generateSiuCalcrickName,
+    stringCompression: true,
+    stringEncoding: true,
+    stringSplitting: true,
+    controlFlowFlattening: 0.95,
+    shuffle: true,
+    rgf: false,
+    flatten: true,
+    duplicateLiteralsRemoval: true,
+    deadCode: true,
+    calculator: true,
+    opaquePredicates: true,
+    lock: {
+      selfDefending: true,
+      antiDebug: true,
+      integrity: true,
+      tamperProtection: true,
+    },
+  };
+};
+
+module.exports = {
+  getMvaaObfuscationConfig,
+};
